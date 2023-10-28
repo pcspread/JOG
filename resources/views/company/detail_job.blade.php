565635936c2c18eb659a55eecda5ee964d63a02d
pcspread <drummer0125vocal@sea.chukai.ne.jp>
2023-10-28 18:35:08 +0900

    creating update

--- resources/views/company/detail_job.blade.php
+++ resources/views/company/detail_job.blade.php
@@ -7,48 +7,46 @@
 @section('content')
 <div class="detail-section">
     <div class="top-group">
-        <h1 class="detail-title">コンサートスタッフ</h1>
+        <h1 class="detail-title">{{ $job->genre['name'] }}</h1>
         <div class="detail-buttons">
             <a class="detail-edit" href="/company/detail/1/edit">修正する</a>
             <a class="detail-back" href="/company">戻る</a>
         </div>
     </div>
     <div class="bottom-group">
-        <p class="job-description">
-            コンサート会場の設営、撤去、運営と、グッズ販売、片付けを行ってもらいます。勤務時間は2パターンに分かれていて、お好きな方を選んでいただけますが、人数が少ない方にお願いすることがあります。設営作業や撤去作業など、力仕事も多く、元気な方大歓迎です。
-        </p>
+        <p class="job-description">{{ $job['content'] }}</p>
         <div class="job-group">
             <div class="job-wrapper first">
                 <div class="job-item">
-                    <label class="job-label">給料：</label>
-                    <p class="job-content">時給1000～1500円</p>
+                    <label class="job-label">給料</label>
+                    <p class="job-content">{{ $job['salary'] }}</p>
                 </div>
                 <div class="job-item">
-                    <label class="job-label">勤務時間：</label>
-                    <p class="job-content">①9:00～16:00、②16:00～23:00(休憩1.5h)</p>
+                    <label class="job-label">勤務時間</label>
+                    <p class="job-content">{{ $job['time'] }}</p>
                 </div>
                 <div class="job-item">
-                    <label class="job-label">シフト：</label>
-                    <p class="job-content">週1日～</p>
+                    <label class="job-label">シフト</label>
+                    <p class="job-content">{{ $job['shift'] }}</p>
                 </div>
             </div>
 
             <div class="job-wrapper second">
                 <div class="job-item">
-                    <label class="job-label">店舗名：</label>
-                    <p class="job-content">店舗A</p>
+                    <label class="job-label">店舗名</label>
+                    <p class="job-content">{{ $job['name'] }}</p>
                 </div>
                 <div class="job-item">
-                    <label class="job-label">勤務場所：</label>
-                    <p class="job-content">千代田区</p>
+                    <label class="job-label">勤務場所</label>
+                    <p class="job-content">{{ $job['location'] }}</p>
                 </div>
                 <div class="job-item">
-                    <label class="job-label">電話番号：</label>
-                    <p class="job-content">08011111111</p>
+                    <label class="job-label">電話番号</label>
+                    <p class="job-content">{{ $job['tel'] }}</p>
                 </div>
                 <div class="job-item">
-                    <label class="job-label">メールアドレス：</label>
-                    <p class="job-content">campanyA@item.com</p>
+                    <label class="job-label">メールアドレス</label>
+                    <p class="job-content">{{ $job['email'] }}</p>
                 </div>
             </div>
         </div>
--- resources/views/company/edit_job.blade.php
+++ resources/views/company/edit_job.blade.php
@@ -6,67 +6,75 @@
 
 @section('content')
 <div class="applicant-section">
-    <form class="applicant-form">
+    <form class="applicant-form" action="/company/detail/{{ $job['id'] }}/edit" method="POST">
+    @csrf
         <h1 class="applicant-title">求人修正</h1>
         <div class="applicant-item">
             <label class="applicant-item__title">ジャンル</label>
             <select class="applicant-item__content" name="gender">
-                <option class="applicant-item__option" value="mail">コンサートスタッフ</option>
-                <option class="applicant-item__option" value="femail">イベントスタッフ</option>
+                @foreach ($genres as $genre)
+                <option class="applicant-item__option" value="{{ $genre['id'] }}">{{ $genre['name'] }}</option>
+                @endforeach
             </select>
+            <p class="applicant-item__error"></p>
+        </div>
+        <div class="applicant-item">
+            <label class="applicant-item__title" for="new_genre">ジャンル(上記に無いジャンルの場合)</label>
+            <input class="applicant-item__content" id="new_genre" type="text" name="new_genre" value="{{ $job['new_genre'] }}" placeholder="入力欄">
             <p class="applicant-item__error"></p>
         </div>
         <div class="applicant-item">
             <label class="applicant-item__title">勤務地</label>
             <select class="applicant-item__content" name="gender">
-                <option class="applicant-item__option" value="mail">千代田区</option>
-                <option class="applicant-item__option" value="femail">渋谷区</option>
+                @foreach ($areas as $area)
+                <option class="applicant-item__option" value="{{ $area['id'] }}">{{ $area['name'] }}</option>
+                @endforeach
             </select>
             <p class="applicant-item__error"></p>
         </div>
         <div class="applicant-item">
             <label class="applicant-item__title" for="name">企業名</label>
-            <input class="applicant-item__content" id="name" type="text" name="name" value="{{ old('name') }}" placeholder="入力欄">
+            <input class="applicant-item__content" id="name" type="text" name="name" value="{{ $job['name'] }}" placeholder="入力欄">
             <p class="applicant-item__error"></p>
         </div>
         <div class="applicant-item">
             <label class="applicant-item__title" for="content">仕事の内容</label>
-            <textarea class="applicant-item__content textarea" name="content" id="content" cols="30" rows="7" placeholder="入力欄">{{ old('content') }}</textarea>
+            <textarea class="applicant-item__content textarea" name="content" id="content" cols="30" rows="7" placeholder="入力欄">{{ $job['content'] }}</textarea>
             <p class="applicant-item__error"></p>
         </div>
         <div class="applicant-item">
             <label class="applicant-item__title" for="email">メールアドレス</label>
-            <input class="applicant-item__content" id="email" type="text" name="email" value="{{ old('email') }}" placeholder="入力欄" readonly>
+            <input class="applicant-item__content" id="email" type="text" name="email" value="{{ $job['email'] }}" placeholder="入力欄" readonly>
             <p class="applicant-item__error"></p>
         </div>
         <div class="applicant-item">
             <label class="applicant-item__title" for="tel">電話番号</label>
-            <input class="applicant-item__content" id="tel" type="text" name="tel" value="{{ old('tel') }}" placeholder="入力欄">
+            <input class="applicant-item__content" id="tel" type="text" name="tel" value="{{ $job['tel'] }}" placeholder="入力欄">
             <p class="applicant-item__error"></p>
         </div>
         <div class="applicant-item">
             <label class="applicant-item__title" for="salary">給料</label>
-            <input class="applicant-item__content" id="salary" type="text" name="salary" value="{{ old('salary') }}" placeholder="入力欄">
+            <input class="applicant-item__content" id="salary" type="text" name="salary" value="{{ $job['salary'] }}" placeholder="入力欄">
             <p class="applicant-item__error"></p>
         </div>
         <div class="applicant-item">
             <label class="applicant-item__title" for="time">勤務時間</label>
-            <input class="applicant-item__content" id="time" type="text" name="time" value="{{ old('time') }}" placeholder="入力欄">
+            <input class="applicant-item__content" id="time" type="text" name="time" value="{{ $job['time'] }}" placeholder="入力欄">
             <p class="applicant-item__error"></p>
         </div>
         <div class="applicant-item">
             <label class="applicant-item__title" for="shift">シフト</label>
-            <input class="applicant-item__content" id="shift" type="text" name="shift" value="{{ old('shift') }}" placeholder="入力欄">
+            <input class="applicant-item__content" id="shift" type="text" name="shift" value="{{ $job['shift'] }}" placeholder="入力欄">
             <p class="applicant-item__error"></p>
         </div>
         <div class="applicant-item">
             <label class="applicant-item__title" for="location">勤務場所</label>
-            <input class="applicant-item__content" id="location" type="text" name="location" value="{{ old('location') }}" placeholder="入力欄">
+            <input class="applicant-item__content" id="location" type="text" name="location" value="{{ $job['location'] }}" placeholder="入力欄">
             <p class="applicant-item__error"></p>
         </div>
         <div class="applicant-buttons">
             <button class="applicant-button">応募する</button>
-            <a class="applicant-button back" href="/company/detail/1">戻る</a>
+            <a class="applicant-button back" href="/company/detail/{{ $job['id'] }}">戻る</a>
         </div>
     </form>
 </div>
